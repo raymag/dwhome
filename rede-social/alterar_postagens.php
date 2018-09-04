0f9319724456cb98492795e4792a0e6739f62465
raymag <prayk11@outlook.com>
2018-09-04 16:45:35 -0300

    .

--- rede-social/alterar_postagens.php
+++ rede-social/alterar_postagens.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION["codigo"])){
+	header("location:index.php");
+}
+?>
 <!DOCTYPE html!>
 <html lang="pt-br">
 <head>
@@ -15,7 +21,24 @@
 			<?php print"Hoje é ".date("d/M/Y").", horário atual: ".date("H:i");?>
 		</div>
 		<?php
-			//Inserir Código para alterar postagens aqui...
+			$id_usuario = $_SESSION["codigo"];
+			include "conexao.php";
+			$conn = conecta_mysql();
+			$sql = "SELECT * FROM postagem WHERE id_usuario = '$id_usuario' ORDER BY data_inclusao DESC";
+			$query = mysqli_query($conn, $sql);
+			$msgs = array();
+			while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)){
+				$msgs[] = $row;
+			}
+			foreach($msgs as $msg){
+				echo "<div id='postagem' class='clear tamanho-450'>";
+				echo "Código da Postagem: ".$msg["id_postagem"];
+				echo "<br>Código do Usuário: ".$msg["id_usuario"];
+				echo "<br>Texto do Postagem: ".$msg["texto_postagem"];
+				echo "<br>Data da Postagem: ".$msg["data_inclusao"];
+				echo "<br><a href='alterar_postagens_confirmar.php?id=".$msg["id_postagem"]."'>Alterar</a>";
+				echo "</div>";
+			}
 			//cada postagem deverá ter o link para alterar a postagem, passando o id para o arquivo alterar_postagens_confirmar.php
 		?>
 	</div> <!-- Div Área principal -->
--- rede-social/alterar_postagens_confirmar.php
+++ rede-social/alterar_postagens_confirmar.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION["codigo"])){
+	header("location:erro.php?er=2");
+}
+?>
 <!DOCTYPE html!>
 <html lang="pt-br">
 <head>
@@ -12,13 +18,42 @@
 	<?php include "includes/menu-login.php" ?>
 	<div id="div-area-principal">
 		<?php
+		include "conexao.php";
+		$conn = conecta_mysql();
+		if(isset($_GET["id"])){
+			$id = $_GET["id"];
+
+			$sql = "SELECT * FROM postagem WHERE id_postagem = '$id'";
+			// echo $sql;
+			if($query = mysqli_query($conn, $sql)){
+				$msg = mysqli_fetch_array($query);
+				// echo "<div id='postagem' class='clear tamanho-450'>";
+				// echo "Código da Postagem: ".$msg["id_postagem"];
+				// echo "<br>Código do Usuário: ".$msg["id_usuario"];
+				// echo "<br>Texto do Postagem: ".$msg["texto_postagem"];
+				// echo "<br>Data da Postagem: ".$msg["data_inclusao"];
+				// echo "</div>";
+			}
+		}
 		//listar a mensagem pelo código recebido aqui
 		?>
 		<div id="postagem">
-			Criar o Formulário com a mensagem a ser alterado aqui
+			<form method="post">
+			<label>Mensagem: <br><textarea name="msg" id="txtarea" autofocus style="resize:none;" cols="30" rows="8"><?php echo $msg["texto_postagem"];
+			?> </textarea></label><br>
+			<input type="submit" value="Alterar">
+			<input type="reset" onclick="document.getElementById('txtarea').innerHTML = ''" value="Cancelar">
+			</form>
 		</div>
 			<?php
-			//código para alterar a mensagem do formulário aqui
+			if(isset($_POST["msg"])){
+				$text = $_POST["msg"];
+				$sql = "UPDATE postagem SET texto_postagem = '$text' WHERE id_postagem = '".$msg["id_postagem"]."'";
+				if(mysqli_query($conn, $sql)){
+					echo "<script>alert('Mensagem alterada com sucesso')</script>";
+					header("Location:minhas_postagens.php");
+				}
+			}
 
 			?>
 	</div> <!-- Div Área principal -->
